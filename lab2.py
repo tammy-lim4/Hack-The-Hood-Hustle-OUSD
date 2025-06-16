f146c3d17465adbfce3400bdacc719fecce69608
Tammy Lim <tammyt2708@gmail.com>
2025-06-16 13:30:11 -0700

    completed lab2

---
+++ lab2.py
@@ -0,0 +1,22 @@
+# Lab 2: Python Profiles
+
+## Step 1
+name = "Tammy Lim"
+print(name)
+
+## Step 2
+preferred_name = "Tammy"
+print(preferred_name)
+
+## Step 3
+age = 16
+print(age)
+
+## Step 4
+Have_you_used_python_before = False
+
+## Step 5
+hobbies = ["Games", "Sleeping", "Music"]
+
+## Step 6
+favorite_things = {"food": "chicken alfredo", "color": "blue", "tv show": "breaking bad"}
