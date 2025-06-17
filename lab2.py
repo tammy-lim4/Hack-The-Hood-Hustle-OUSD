258879c6e9d91f6a06ddc705b1475992a7cb6c39
Tammy Lim <tammyt2708@gmail.com>
2025-06-17 16:57:04 -0700

    lab2 completed

--- lab2.py
+++ lab2.py
@@ -2,21 +2,31 @@
 
 ## Step 1
 name = "Tammy Lim"
-print(name)
 
 ## Step 2
 preferred_name = "Tammy"
-print(preferred_name)
 
 ## Step 3
 age = 16
-print(age)
 
-## Step 4
+## Step 4 Boolean variable
 Have_you_used_python_before = False
 
-## Step 5
+## Step 5 List variable
 hobbies = ["Games", "Sleeping", "Music"]
 
-## Step 6
+## Step 6 Dictionary variable
 favorite_things = {"food": "chicken alfredo", "color": "blue", "tv show": "breaking bad"}
+
+## Step 7: Set variable
+teachers = {"joe", "joe", "bob"}
+
+## Step 7: Tuple variable
+teachers1 = ("joe", "joe", "bob")
+
+print(name + " " + preferred_name)
+print(hobbies)
+print(age)
+print(favorite_things)
+print(teachers)
+print(teachers1)
